593d48c54db7c416ecfccd6403031cd901caa0fa
Kishor Pandya <pandyakishor22@gmail.com>
2024-09-25 19:11:17 +0000

    Fixed Bug6

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,12 @@ export function App() {
           {transactions !== null && (
             <button
               className="RampButton"
-              disabled={paginatedTransactionsUtils.loading}
+              disabled={
+                // Fixed Bug6
+                paginatedTransactionsUtils.loading ||
+                paginatedTransactions?.nextPage == null ||
+                transactionsByEmployee?.length === 0
+              }
               onClick={async () => {
                 await loadAllTransactions()
               }}
