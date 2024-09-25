a1f5e6fddffbc241f7933d838a43be7d22a1a00f
Kishor Pandya <pandyakishor22@gmail.com>
2024-09-25 18:41:57 +0000

    Fixed Bug5

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,9 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
-    await paginatedTransactionsUtils.fetchAll()
-
+    // Fixed Bug 5
     setIsLoading(false)
+    await paginatedTransactionsUtils.fetchAll()
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
